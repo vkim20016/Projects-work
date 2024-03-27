a05a523a6351d6e006493df2f0bc6f99a9b6e588
vkim20016 <164546646+vkim20016@users.noreply.github.com>
2024-03-27 13:07:14 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -6,7 +6,90 @@ import tempfile
 from openpyxl import Workbook, load_workbook
 import subprocess
 subprocess.call(['pip', 'install', '-r', 'https://raw.githubusercontent.com/vkim20016/Project-work/main/requirements.txt'])
+from langchain_community.document_loaders import UnstructuredExcelLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from langchain.chains import ConversationalRetrievalChain
+import replicate
+import os
 
+# Define the path for generated embeddings
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+ # Load Excel data using UnstructuredExcelLoader
+    loader = UnstructuredExcelLoader(file_path=tmp_file_path, mode="elements")
+    docs = loader.load()
+    doc = docs[0]  # Access the first document in the list
+# Create embeddings using Sentence Transformers
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+
+    # Create a FAISS vector store and save embeddings
+    db = FAISS.from_documents(data, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+# Refactored from https://github.com/a16z-infra/llama2-chatbot
+# Models and parameters
+st.subheader('Models and parameters')
+selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
+
+if selected_model == 'Llama2-7B':
+    llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+elif selected_model == 'Llama2-13B':
+    llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+else:
+    llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
+
+temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+
+# Create a conversational chain
+chain = ConversationalRetrievalChain.from_llm(llm=llm, temperature=temperature, top_p=top_p, max_length=max_length)
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
 def filter_dataframe(df: pd.DataFrame, filter_columns: list) -> pd.DataFrame:
     filtered_df = df.copy()
 
@@ -43,9 +126,21 @@ st.set_page_config(
     page_icon=":bar_chart:",
     layout="wide"
 )
-
+# Set the title for the Streamlit app
+st.title("Medical Insights Chatbot")
 # Upload Excel file
 uploaded_file = st.sidebar.file_uploader("Upload Excel file", type=["xlsx"])
+# Replicate Credentials
+with st.sidebar:
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
 
 if uploaded_file is not None:
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -63,93 +158,4 @@ if uploaded_file is not None:
             st.dataframe(df)
         except Exception as e:
             st.error(f"An error occurred: {e}")
-            embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
-from langchain_community.document_loaders import UnstructuredExcelLoader
-import streamlit as st
-import tempfile
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import CTransformers
-from langchain.chains import ConversationalRetrievalChain
-
-# Define the path for generated embeddings
-DB_FAISS_PATH = 'vectorstore/db_faiss'
-
-# Load the model of choice
-def load_llm():
-    llm = CTransformers(
-        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-        model_type="llama",
-        max_new_tokens=512,
-        temperature=0.5
-    )
-    return llm
-
-# Set the title for the Streamlit app
-st.title("Llama2 Chat Excel - 🦜🦙")
-
-# Create a file uploader in the sidebar
-uploaded_file = st.sidebar.file_uploader("Upload File", type=["csv", "xlsx"])
-
-# Handle file upload
-if uploaded_file:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
-
-    # Load Excel data using UnstructuredExcelLoader
-    loader = UnstructuredExcelLoader(file_path=tmp_file_path, mode="elements")
-    docs = loader.load()
-    doc = docs[0]  # Access the first document in the list
-
-    # Create embeddings using Sentence Transformers
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
-
-    # Create a FAISS vector store and save embeddings
-    db = FAISS.from_documents([doc], embeddings)  # Pass the document as a list
-    db.save_local(DB_FAISS_PATH)
-
-    # Load the language model
-    llm = load_llm()
-
-    # Create a conversational chain
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
-
-    # Function for conversational chat
-    def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"]
-
-    # Initialize chat history
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
-
-    # Initialize messages
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! Ask me (LLAMA2) about " + uploaded_file.name + " 🤗"]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey! 👋"]
-
-    # Create containers for chat history and user input
-    response_container = st.container()
-    container = st.container()
-
-    # User input form
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Query:", placeholder="Talk to Excel data 👉 (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-    # Display chat history
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                st.write(st.session_state["past"][i], key=str(i) + '_user')
-                st.write(st.session_state["generated"][i], key=str(i))
+  
