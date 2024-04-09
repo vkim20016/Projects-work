95c3ff48e6c2fb81625887b0953194c7c5cddb1b
vkim20016 <164546646+vkim20016@users.noreply.github.com>
2024-04-09 19:40:11 -0400

    Add files via upload

--- cookie.py
+++ cookie.py
@@ -1,88 +1,147 @@
 # Import necessary libraries
 import streamlit as st
 import tempfile
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import CTransformers
-from langchain.chains import ConversationalRetrievalChain
-# Define the path for generated embeddings
-DB_FAISS_PATH = 'vectorstore/db_faiss'
+from langchain.agents import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+import pandas as pd
+import os
+from pandas.api.types import is_object_dtype, is_datetime64_any_dtype
+from pandas.api.types import CategoricalDtype
+from langchain_experimental.agents import create_pandas_dataframe_agent
 
-# Load the model of choice
-def load_llm():
-    llm = CTransformers(
-        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-        model_type="llama",
-        max_new_tokens=512,
-        temperature=0.5
+
+
+def filter_dataframe(df: pd.DataFrame, filter_columns: list) -> pd.DataFrame:
+    filtered_df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in filtered_df.columns:
+        if is_object_dtype(filtered_df[col]):
+            try:
+                filtered_df[col] = pd.to_datetime(filtered_df[col])
+            except Exception:
+                pass
+        if is_datetime64_any_dtype(filtered_df[col]):
+            filtered_df[col] = filtered_df[col].dt.tz_localize(None)
+
+    for column in filter_columns:
+        with st.expander(f"Filter by {column}", expanded=False):
+            if isinstance(filtered_df[column].dtype, CategoricalDtype) or filtered_df[column].nunique() < 10000:
+                unique_values = filtered_df[column].unique()
+                selected_values = st.multiselect(
+                    f"Values for {column}",
+                    unique_values,
+                    default=[],
+                )
+                if len(selected_values) > 0:
+                    filtered_df = filtered_df[filtered_df[column].isin(selected_values)]
+
+    return filtered_df
+
+file_formats = {
+    "csv": pd.read_csv,
+    "xls": pd.read_excel,
+    "xlsx": pd.read_excel,
+    "xlsm": pd.read_excel,
+    "xlsb": pd.read_excel,
+}
+
+
+
+
+def clear_submit():
+    """
+    Clear the Submit Button State
+    Returns:
+
+    """
+    st.session_state["submit"] = False
+
+
+@st.cache_data(ttl="2h")
+def load_data(uploaded_file):
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    else:
+        st.error(f"Unsupported file format: {ext}")
+        return None
+    st.set_page_config(page_title="LangChain: Chat with pandas DataFrame", page_icon="🦜")
+st.title("🦜 LangChain: Chat with pandas DataFrame")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload a Data file",
+    type=list(file_formats.keys()),
+    help="Various File formats are Support",
+    on_change=clear_submit,
+)
+if uploaded_file is not None:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_file_path = f"{tmp_dir}/uploaded_file"
+        with open(tmp_file_path, "wb") as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+
+        try:
+            file_extension = os.path.splitext(uploaded_file.name)[1][1:]
+            if file_extension in file_formats:
+                df = file_formats[file_extension](tmp_file_path)
+                filter_columns = st.sidebar.multiselect("Filter dataframe on", df.columns, key="filter_columns")
+
+                if len(filter_columns) > 0:
+                    df = filter_dataframe(df, filter_columns)
+
+                st.dataframe(df)
+            else:
+                st.error("Unsupported file format. Please upload a CSV or Excel file.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+if not uploaded_file:
+    st.warning(
+        "This app uses LangChain's `PythonAstREPLTool` which is vulnerable to arbitrary code execution. Please use caution in deploying and sharing this app."
     )
-    return llm
 
-# Set the title for the Streamlit app
-st.title("Llama2 Chat CSV - 🦜🦙")
+if not uploaded_file:
+    st.warning(
+        "This app uses LangChain's `PythonAstREPLTool` which is vulnerable to arbitrary code execution. Please use caution in deploying and sharing this app."
+    )
 
-# Create a file uploader in the sidebar
-uploaded_file = st.sidebar.file_uploader("Upload File", type="csv")
-
-# Handle file upload
 if uploaded_file:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+    df = load_data(uploaded_file)
 
-    # Load CSV data using CSVLoader
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','})
-    data = loader.load()
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-    # Create embeddings using Sentence Transformers
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-    # Create a FAISS vector store and save embeddings
-    db = FAISS.from_documents(data, embeddings)
-    db.save_local(DB_FAISS_PATH)
+if prompt := st.chat_input(placeholder="What is this data about?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-    # Load the language model
-    llm = load_llm()
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-    # Create a conversational chain
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+    llm = ChatOpenAI(
+        temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=openai_api_key, streaming=True
+    )
 
-    # Function for conversational chat
-    def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"]
+    pandas_df_agent = create_pandas_dataframe_agent(
+        llm,
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        handle_parsing_errors=True,
+    )
 
-    # Initialize chat history
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
-
-    # Initialize messages
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me(LLAMA2) about " + uploaded_file.name + " 🤗"]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
-
-    # Create containers for chat history and user input
-    response_container = st.container()
-    container = st.container()
-
-    # User input form
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Query:", placeholder="Talk to csv data 👉 (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-    # Display chat history
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
